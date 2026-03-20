ca388fa24a53eb1ee9160a3894761ac4c5bdc7e3
tiffsoa <atiff.soa@gmail.com>
2026-03-19 21:55:17 -0400

    added auth library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,4 +45,5 @@ dependencies {
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
     implementation(platform("com.google.firebase:firebase-bom:34.9.0"))
+    implementation(libs.firebase.auth)
 }
