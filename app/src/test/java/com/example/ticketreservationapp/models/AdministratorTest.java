76bed9f7eff7bd82ddb8e251e6537d2382ab5fac
tiffsoa <atiff.soa@gmail.com>
2026-03-24 18:45:05 -0400

    added admin unit test class for admin creation

---
+++ app/src/test/java/com/example/ticketreservationapp/models/AdministratorTest.java
@@ -0,0 +1,21 @@
+package com.example.ticketreservationapp.models;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AdministratorTest {
+
+    @Test
+    public void testAdministratorInitialization() {
+        String expectedId = "admin_999";
+        String expectedEmail = "admin@system.com";
+        String expectedPhone = "+15559990000";
+
+        Administrator testAdmin = new Administrator(expectedId, expectedEmail, expectedPhone);
+
+        assertEquals(expectedId, testAdmin.getUserID(), "Admin ID should match the input");
+        assertEquals(expectedEmail, testAdmin.getEmail(), "Admin Email should match the input");
+        assertEquals(expectedPhone, testAdmin.getPhoneNumber(), "Admin Phone should match the input");
+    }
+
+}
