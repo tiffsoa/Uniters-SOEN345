2a80d5d48bc6c7a24c62a62cec42966c97939aea
KhaledDaoud9 <khaled2510@gmail.com>
2026-04-13 23:19:57 -0400

    Create EventRepositoryTest.java

---
+++ app/src/test/java/com/example/ticketreservationapp/EventRepositoryTest.java
@@ -0,0 +1,12 @@
+package com.example.ticketreservationapp;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class EventRepositoryTest {
+
+    @Test
+    public void testBasic() {
+        assertTrue(true);
+    }
+}
